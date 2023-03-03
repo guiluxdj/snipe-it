b6f213a2e951b4266f5dd38a06398a7e9196194f
guiluxdj <guiluxdj@gmail.com>
2023-03-03 14:40:51 +0100

    delete footer credit

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -837,9 +837,6 @@
                   {!!  Helper::parseEscapedMarkedown($snipeSettings->footer_text)  !!}
               </div>
           @endif
-          
-
-          <a target="_blank" href="https://snipeitapp.com" rel="noopener">Snipe-IT</a> is open source software, made with <i class="fas fa-heart" style="color: #a94442; font-size: 10px" aria-hidden="true"></i><span class="sr-only">love</span> by <a href="https://twitter.com/snipeitapp" rel="noopener">@snipeitapp</a>.
       </footer>
 
 
