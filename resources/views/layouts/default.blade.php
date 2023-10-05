478c809ec04c750d8238d82d247f343f1f6311fa
guiluxdj <guiluxdj@gmail.com>
2023-10-05 09:34:59 +0200

    footer text

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -853,9 +853,7 @@
 
                 <div class="1hidden-xs pull-left">
                     <div class="pull-left" >
-                        <a target="_blank" href="https://snipeitapp.com" rel="noopener">Snipe-IT</a> is open source software,
-                        made with <i class="fas fa-heart" style="color: #a94442; font-size: 10px" aria-hidden="true"></i><span
-                                class="sr-only">love</span> by <a href="https://twitter.com/snipeitapp" rel="noopener">@snipeitapp</a>.
+                       <a target="_blank" href="http://walit.walcourt.local" rel="noopener">WAL-IT</a> Gestion des actifs </a>.
                     </div>
                     <div class="pull-right">
                     @if ($snipeSettings->version_footer!='off')
@@ -865,7 +863,7 @@
                         @endif
                     @endif
 
-                    @if ($snipeSettings->support_footer!='off')
+                    {{-- @if ($snipeSettings->support_footer!='off')
                         @if (($snipeSettings->support_footer=='on') || (($snipeSettings->support_footer=='admin') && (Auth::user()->isSuperUser()=='1')))
                             <a target="_blank" class="btn btn-default btn-xs"
                                href="https://snipe-it.readme.io/docs/overview"
@@ -879,7 +877,7 @@
                         <a target="_blank" class="btn btn-default btn-xs" rel="noopener"
                            href="{{  $snipeSettings->privacy_policy_link }}"
                            target="_new">{{ trans('admin/settings/general.privacy_policy') }}</a>
-                    @endif
+                    @endif --}}
                     </div>
                     <br>
                     @if ($snipeSettings->footer_text!='')
