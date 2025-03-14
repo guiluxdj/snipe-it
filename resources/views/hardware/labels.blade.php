3fda981d556760eedc476274275926d4e726669f
Guilux <guiluxdj@gmail.com>
2025-03-14 09:04:23 +0100

    fix label

--- resources/views/hardware/labels.blade.php
+++ resources/views/hardware/labels.blade.php
@@ -265,9 +265,9 @@
                     <div class="pull-left">
                         Livraison: <strong> 
                            @if ($asset->assignedToUser)
-                                {{ $asset->assignedTouser->first_name . '' . $asset->assignedTouser->last_name}}
+                                {{ $asset->assignedTouser->first_name . ' ' . $asset->assignedTouser->last_name}}
                             @endif
-                            ({{ $asset->last_checkout->format('d/m/Y') }})</strong>
+                            ({{ \Carbon\Carbon::parse($asset->last_checkout)->format('d/m/Y') }})</strong>
                     </div>
                 @else
                     @if ($asset->purchase_date)
